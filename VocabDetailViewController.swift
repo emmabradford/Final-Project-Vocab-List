0f632f483c2719a1ab42a9248d8e8c1c90bc41f5
ajakopin <amyjakopin@gmail.com>
2015-05-13 09:48:18 -0500

    image picker in progress

--- VocabDetailViewController.swift
+++ VocabDetailViewController.swift
@@ -14,17 +14,28 @@ class VocabDetailViewController: UIViewController {
     @IBOutlet weak var vocabWordDeffinitionTextField: UITextField!
     @IBOutlet weak var vocabWordNotesTextField: UITextField!
     @IBOutlet weak var vocabImageView: UIImageView!
-    
-    
-    
-    
+    let imagePicker = UIImagePickerController()
+    var image = UIImage(named: "default")
     
     override func viewDidLoad() {
+        vocabImageView.image = image
         super.viewDidLoad()
     }
     @IBAction func onWedsiteTappedButton(sender: UIButton) {
     }
     @IBAction func onSavedButtonTapped(sender: UIButton) {
     }
-
+    @IBAction func onLibraryButtonTapped(sender: UIButton) {
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+ //   func imagePickerController(
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: {
+            var selectedImage = info [UIImagePickerControllerOriginalImage] as! UIImage
+            self.vocabImageView.image = selectedImage
+        })
+    }
 }
